df64d62a1f3651c0df5de3bfd42adc315b2158c3
kuria  githinji <kurialancelot@gmail.com>
2024-02-23 18:28:48 +0300

    pushing everything to allow for intellij setup

---
+++ pusher.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+# Navigate to the directory containing your local Git repository
+cd "$(dirname "$0")"
+branch='main'
+
+# Add all changes to the Git staging are
+git add .
+
+#check status of files to be committed
+git status
+
+# Commit the changes with a descriptive message
+git commit -m "pushing everything to allow for intellij setup"
+
+# Push the changes to your GitHub repository
+git push origin $branch
